4caea51f0ba7ff854f8886ea28de8f9f8d485324
Anton Lukyanchikov <antontotenkopf@gmail.com>
2024-02-07 20:22:54 +0300

    update App

--- src/App.java
+++ src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) {
         System.out.println("Открытие магазина ожидается через 4 дня");
-        System.out.println("но это не точно");
+        System.out.println("но может быть завтра");
     }
 }
